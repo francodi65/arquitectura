c0754454d5ff3b780e1d0cd50895be533ac42d54
francodi65 <franco_di_65@hotmail.com>
2017-09-26 12:55:12 -0300

    uart libro

---
+++ tp2/baud_gen.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:04:09 09/26/2017 
+// Design Name: 
+// Module Name:    baud_gen 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module baud_gen(
+    );
+
+
+endmodule
