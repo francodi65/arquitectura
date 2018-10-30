f7a174e52d12c946cda301d3afceeeb285790acb
Franco <franco_di_65@hotmail.com>
2018-10-29 21:13:38 -0300

    adder

---
+++ tp4/adder.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:06:00 10/29/2018 
+// Design Name: 
+// Module Name:    adder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module adder(
+				input [31:0] adder_in,
+				output [31:0] adder_out
+				);
+				
+	assign adder_out = adder_in + 1;
+
+
+
+endmodule
