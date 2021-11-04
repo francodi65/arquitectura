3227e7be66510ca14dd04f1095118dadcfb1b596
Franco <franco_di_65@hotmail.com>
2021-11-04 00:06:21 -0300

    ultimos cambios

--- tp4/decoder.v
+++ tp4/decoder.v
@@ -33,10 +33,11 @@ module decoder#(
 				);
 
 //---------- Local variables declaration ----------//
-
+	//wire execute_bus[3:0][0];
+	
 	// Execute bus bits
 	localparam  [EXEC_BUS_WIDTH-1:0]
-		alu_op[3:0]	= [3:0],
+		//execute_bus[3:0]  = [0,3],
 		alu_src  	=  4, 
 		reg_dst   	=  5;
 	
@@ -50,9 +51,8 @@ module decoder#(
 	localparam  [WB_BUS_WIDTH-1:0]
 		mem_to_reg 	=  0, 
 		reg_write  	=  1;
-	
-	wire alu_op;
-	assign alu_op = execute_bus[3:0];
+		
+	localparam zero = 0;
 		
 //-----------------------------------------------//
 
@@ -67,20 +67,20 @@ module decoder#(
 					3'b000: // R-type
 					begin
 						case(funct[5:0])
-							 6'b000000: alu_op <= 4'b0000;
-							 6'b000010: alu_op <= 4'b0001;
-							 6'b000011: alu_op <= 4'b0010;
-							 6'b000100: alu_op <= 4'b0000;
-							 6'b000110: alu_op <= 4'b0001;
-							 6'b000111: alu_op <= 4'b0010;
-							 6'b100001: alu_op <= 4'b0011;
-							 6'b100011: alu_op <= 4'b1000;
-							 6'b100100: alu_op <= 4'b0100;
-							 6'b100101: alu_op <= 4'b0101;
-							 6'b100110: alu_op <= 4'b0110;
-							 6'b100111: alu_op <= 4'b0111;
-							 6'b101010: alu_op <= 4'b1001;
-							 default:   alu_op <= 4'b1111;
+							 6'b000000: execute_bus[3:0] <= 4'b0000;
+							 6'b000010: execute_bus[3:0] <= 4'b0001;
+							 6'b000011: execute_bus[3:0] <= 4'b0010;
+							 6'b000100: execute_bus[3:0] <= 4'b0000;
+							 6'b000110: execute_bus[3:0] <= 4'b0001;
+							 6'b000111: execute_bus[3:0] <= 4'b0010;
+							 6'b100001: execute_bus[3:0] <= 4'b0011;
+							 6'b100011: execute_bus[3:0] <= 4'b1000;
+							 6'b100100: execute_bus[3:0] <= 4'b0100;
+							 6'b100101: execute_bus[3:0] <= 4'b0101;
+							 6'b100110: execute_bus[3:0] <= 4'b0110;
+							 6'b100111: execute_bus[3:0] <= 4'b0111;
+							 6'b101010: execute_bus[3:0] <= 4'b1001;
+							 default:   execute_bus[3:0] <= 4'b1111;
 						endcase
 						case(funct[5:0])
 							 6'b001000, 
@@ -121,7 +121,7 @@ module decoder#(
 			3'b100,//Loads
 			3'b101://Stores 
 			begin
-				alu_op <= 4'b0011;
+				execute_bus[3:0] <= 4'b0011;
 				if(opcode[3])
 				begin
 					memory_bus[mem_read] <= 0;
@@ -141,14 +141,14 @@ module decoder#(
 			end
 			3'b001://Inmediates
 			begin
-				case(inst[28:26])
-					3'b000: alu_op <= 4'b0011;
-					3'b100: alu_op <= 4'b0100;
-					3'b101: alu_op <= 4'b0101;
-					3'b110: alu_op <= 4'b0110;
-					3'b111: alu_op <= 4'b0000;
-					3'b010: alu_op <= 4'b1001;
-					default: alu_op <= 4'b1111;
+				case(opcode[2:0])
+					3'b000: execute_bus[3:0] <= 4'b0011;
+					3'b100: execute_bus[3:0] <= 4'b0100;
+					3'b101: execute_bus[3:0] <= 4'b0101;
+					3'b110: execute_bus[3:0] <= 4'b0110;
+					3'b111: execute_bus[3:0] <= 4'b0000;
+					3'b010: execute_bus[3:0] <= 4'b1001;
+					default: execute_bus[3:0] <= 4'b1111;
 				endcase
 				wb_bus[reg_write] <= 1;
 				execute_bus[reg_dst] <= 1;//By default rt
@@ -160,7 +160,7 @@ module decoder#(
 			end        
 			default:
 			begin
-				alu_op <= 4'b1111;
+				execute_bus[3:0] <= 4'b1111;
 				execute_bus[reg_dst] <= 1;//By default rt
 				mux_inst <= 0; //By default PC.
 				memory_bus[mem_read] <= 0; //By default read disabled.
