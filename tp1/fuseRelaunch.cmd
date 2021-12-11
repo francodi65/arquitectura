af6949175767a1a2ca7717302fd4faf0b75b005f
Franco <franco_di_65@hotmail.com>
2021-12-11 19:30:57 -0300

    removing gitignore files

--- tp1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/Juanma/arquitectura/tp1/test_isim_beh.exe" -prj "C:/Users/Juanma/arquitectura/tp1/test_beh.prj" "work.test" "work.glbl" 
--- tp3/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Franco/Git/arquitectura/tp3/bip_test_isim_beh.exe" -prj "C:/Users/Franco/Git/arquitectura/tp3/bip_test_beh.prj" "work.bip_test" "work.glbl" 
